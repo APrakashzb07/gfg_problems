8ce3a7f8a3c409bc6bb22f2cc7f05c50bfef817e
ANAND PRAKASH <121022698+APrakashzb07@users.noreply.github.com>
2024-09-23 11:44:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Missing And Repeating/missing-and-repeating.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().split(" ");
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            int[] ans = new Solve().findTwoElement(a);
+            System.out.println(ans[0] + " " + ans[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solve {
+    int[] findTwoElement(int arr[]) {
+        // code here
+         int n=arr.length;
+        int[] res=new int[2];
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        for(int i=1;i<=n;i++){
+            if(map.get(arr[i-1])>1) res[0]=arr[i-1];
+            if(!map.containsKey(i)) res[1]=i;
+            
+        }
+        return res;
+    }
+}
