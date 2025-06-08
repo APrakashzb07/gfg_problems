c88cf434995dfd20e8208821006e2d15677500b4
ANAND PRAKASH <121022698+APrakashzb07@users.noreply.github.com>
2025-06-08 13:16:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Second Largest/second-largest.cs
@@ -0,0 +1,25 @@
+class Solution {
+    public int GetSecondLargest(int[] arr) {
+        // code here
+        int n = arr.Length;
+
+        int largest = -1, secondLargest = -1;
+
+        // Finding the largest element
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest)
+                largest = arr[i];
+        }
+
+        // Finding the second largest element
+        for (int i = 0; i < n; i++) {
+            
+            // Update second largest if the current element is greater
+            // than second largest and not equal to the largest
+            if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+}
